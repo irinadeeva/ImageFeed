b7fe926d494b2181db9b7e48177353c24375977a
irinadeeva <51794769+irinadeeva@users.noreply.github.com>
2023-11-30 18:28:35 +0100

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Irina Deeva on 30/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

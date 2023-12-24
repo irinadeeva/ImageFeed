b0da7df76dc666ac89bb9e267163c8858b421990
irinadeeva <deeva97@me.com>
2023-12-24 19:23:40 +0100

    Implemented layout Launch Screen Image scroll view

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -9,42 +9,74 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
     private let photoNames: [String] = Array(0..<20).map{ "\($0)" }
-    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
     @IBOutlet private var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-    
-    func configCell(for: UITableViewCell) {
+
+    func configCell(for cell: ImageListCell, with indexPath: IndexPath) {
+        guard let imageCell = UIImage(named: photoNames[indexPath.row]) else {
+            return
+        }
+        cell.imageCell.image = imageCell
         
+        cell.dataLabel.text =  dateFormatter.string(from: Date())
+        
+        let buttonImage = (indexPath.row % 2) == 0 ? UIImage(named: "Active") : UIImage(named: "No Active")
+        
+        cell.favouriteButton.setImage(buttonImage, for: .normal)
     }
 }
 
 extension ImagesListViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photoNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImageListCell.reuseIndentifier, for: indexPath)
         
         guard let imageListCell = cell as? ImageListCell else {
-            return UITableViewCell() // change
+            return UITableViewCell()
         }
         
-        configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
         return imageListCell
     }
-    
-    
 }
 
 extension ImagesListViewController : UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photoNames[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageWidth = image.size.width
+        let imageHeight = image.size.height
         
+        let requiredWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        
+        let widthRatio = requiredWidth / imageWidth
+        
+        let requiredHeight = imageHeight * widthRatio + imageInsets.top + imageInsets.bottom
+        
+        return requiredHeight
     }
 }
+
+

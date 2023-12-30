1ebeaf343ca05d779260a68df2f25d0326a023fe
irinadeeva <deeva97@me.com>
2023-12-30 16:31:18 +0800

    add navigation and tab bar

---
+++ SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Irina Deeva on 30/12/23.
+//
+
+import Foundation
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    
+}
